c0d9cf8390a9fe77892a9c98b8f397ec5061d072
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-02 15:01:38 +0000

    sss

--- app.py
+++ app.py
@@ -1,3 +1,57 @@
+import requests
+from bs4 import BeautifulSoup
 import streamlit as st
 
-st.write("hello")
+# Define Glassdoor URL and user agent
+url = "https://www.glassdoor.com/Job/jobs.htm"
+headers = {"User-Agent": "Mozilla/5.0"}
+
+# Define Streamlit app
+st.title("Glassdoor Job Scraper")
+
+# Define form inputs
+search_term = st.text_input("Enter a job title")
+location = st.text_input("Enter a location")
+
+# Define data dictionary to store scraped data
+data = {"Job Title": [], "Company Name": [], "Location": [], "Job Link": []}
+
+# Check if form is submitted
+if st.button("Search"):
+    # Define query parameters
+    params = {
+        "keyword": search_term,
+        "location": location,
+        "fromAge": "30d",
+        "jobType": "fulltime",
+    }
+    
+    # Make GET request to Glassdoor URL with query parameters and user agent
+    response = requests.get(url, params=params, headers=headers)
+    
+    # Parse HTML content with BeautifulSoup
+    soup = BeautifulSoup(response.content, "html.parser")
+    
+    # Find all job listings on page
+    job_listings = soup.find_all("li", class_="react-job-listing")
+    
+    # Loop through job listings and extract data
+    for job in job_listings:
+        # Extract job title
+        job_title = job.find("a", class_="jobLink").text
+        data["Job Title"].append(job_title)
+        
+        # Extract company name
+        company_name = job.find("div", class_="jobHeader").find("span").text
+        data["Company Name"].append(company_name)
+        
+        # Extract job location
+        job_location = job.find("div", class_="jobInfoItem jobLocation").text
+        data["Location"].append(job_location)
+        
+        # Extract job link
+        job_link = "https://www.glassdoor.com" + job.find("a", class_="jobLink")["href"]
+        data["Job Link"].append(job_link)
+        
+    # Display scraped data in Streamlit table
+    st.table(data)
