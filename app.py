c9b77ae5a6320016fff88ee452707232c295a3a3
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-02 18:53:41 +0000

    sss

--- app.py
+++ app.py
@@ -1,61 +1,88 @@
-import requests
-from bs4 import BeautifulSoup
-import streamlit as st
+import asyncio
+import json
+import math
+from collections import defaultdict
+from typing import Dict, List
 
-# Define Glassdoor URL and user agent
-url = "https://www.glassdoor.com/Job/jobs.htm"
-headers = {"User-Agent": "Mozilla/5.0"}
+import httpx
+from parsel import Selector
 
-# Define Streamlit app
-st.title("Glassdoor Job Scraper")
+session = httpx.AsyncClient(
+    timeout=httpx.Timeout(30.0),
+    cookies={"tldp": "1"},
+    follow_redirects=True,
+)
 
-# Define form inputs
-search_term = st.text_input("Enter a job title")
-location = st.text_input("Enter a location")
 
-# Define data dictionary to store scraped data
-data = {"Job Title": [], "Company Name": [], "Location": [], "Job Link": []}
+def find_json_objects(text: str, decoder=json.JSONDecoder()):
+    """Find JSON objects in text, and generate decoded JSON data and it's ID"""
+    pos = 0
+    while True:
+        match = text.find("{", pos)
+        if match == -1:
+            break
+        try:
+            result, index = decoder.raw_decode(text[match:])
+            # backtrack to find the key/identifier for this json object:
+            key_end = text.rfind('"', 0, match)
+            key_start = text.rfind('"', 0, key_end)
+            key = text[key_start + 1 : key_end]
+            yield key, result
+            pos = match + index
+        except ValueError:
+            pos = match + 1
 
-# Check if form is submitted
-# if st.button("Search"):
-    # Define query parameters
-params = {
-    "suggestCount": "0",
-"suggestChosen": "false",
-"clickSource": "searchBtn",
-"typedKeyword": "Software",
-"typedLocation": 'United%20States',
-"context": "Jobs",
-"dropdown": "0"
-}
 
-# Make GET request to Glassdoor URL with query parameters and user agent
-response = requests.get(url, params=params, headers=headers)
-st.write(response.content)
-    # Parse HTML content with BeautifulSoup
-    # soup = BeautifulSoup(response.content, "html.parser")
+def extract_apollo_cache(html):
+    """Extract apollo graphql cache data from HTML source"""
+    selector = Selector(text=html)
+    script_with_cache = selector.xpath("//script[contains(.,'window.appCache')]/text()").get()
+    cache = defaultdict(list)
+    for key, data in find_json_objects(script_with_cache):
+        cache[key].append(data)
+    return cache
 
-    
-    # Find all job listings on page
-    # job_listings = soup.find_all("li", class_="react-job-listing")
-    
-    # # Loop through job listings and extract data
-    # for job in job_listings:
-    #     # Extract job title
-    #     job_title = job.find("a", class_="jobLink").text
-    #     data["Job Title"].append(job_title)
-        
-    #     # Extract company name
-    #     company_name = job.find("div", class_="jobHeader").find("span").text
-    #     data["Company Name"].append(company_name)
-        
-    #     # Extract job location
-    #     job_location = job.find("div", class_="jobInfoItem jobLocation").text
-    #     data["Location"].append(job_location)
-        
-    #     # Extract job link
-    #     job_link = "https://www.glassdoor.com" + job.find("a", class_="jobLink")["href"]
-    #     data["Job Link"].append(job_link)
-        
-    # # Display scraped data in Streamlit table
-    # st.table(data)
+
+def parse_jobs(html) -> List[Dict]:
+    """parse jobs page for job data and total amount of jobs"""
+    cache = extract_apollo_cache(html)
+    return [v["jobview"] for v in cache["JobListingSearchResult"]]
+
+
+def parse_job_page_count(html) -> int:
+    """parse job page count from pagination details in Glassdoor jobs page"""
+    _total_results = Selector(html).css(".paginationFooter::text").get()
+    if not _total_results:
+        return 1
+    _total_results = int(_total_results.split()[-1])
+    _total_pages = math.ceil(_total_results / 40)
+    return _total_pages
+
+
+async def scrape_jobs(employer_name: str, employer_id: str):
+    """Scrape job listings"""
+    # scrape first page of jobs:
+    first_page = await session.get(
+        url=f"https://www.glassdoor.com/Jobs/{employer_name}-Jobs-E{employer_id}_P1.htm?filter.countryId={session.cookies.get('tldp') or 0}",
+    )
+    jobs = parse_jobs(first_page.text)
+    total_pages = parse_job_page_count(first_page.text)
+
+    print(f"scraped first page of jobs, scraping remaining {total_pages - 1} pages")
+    other_pages = [
+        session.get(
+            url=str(first_page.url).replace(".htm", f"_P{page}.htm"),
+        )
+        for page in range(2, total_pages + 1)
+    ]
+    for page in await asyncio.gather(*other_pages):
+        jobs.extend(parse_jobs(page.text))
+    return jobs
+
+
+async def main():
+    jobs = await scrape_jobs("eBay", "7853")
+    print(json.dumps(jobs, indent=2))
+
+
+asyncio.run(main())
